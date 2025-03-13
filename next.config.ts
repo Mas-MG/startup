559b58b602ddb47014668236bb8d4a175c24c501
masoomeh ghorbani <masoomeh77ghorbani98@gmail.com>
2025-03-13 14:37:11 +0330

    Update: tailwind config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
 
+};
+module.exports = {
+  // ... other configurations ...
+  experimental: {
+    turbo: {
+      enabled: false,
+    },
+  },
+};
 export default nextConfig;
